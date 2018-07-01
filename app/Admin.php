5353af0241a7197ed962e2e4b2711dd54fab6924
Hilmi Raditya <raditya113@icloud.com>
2018-07-01 19:18:00 +0700

    benerin sistem jancukkkkk

--- app/Admin.php
+++ app/Admin.php
@@ -1,43 +1,43 @@
-    <?php
+<?php
     
-    namespace App;
+namespace App;
     
-    use App\Notifications\AdminResetPassword;
-    use Illuminate\Notifications\Notifiable;
-    use Illuminate\Foundation\Auth\User as Authenticatable;
+use App\Notifications\AdminResetPassword;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
     
-    class Admin extends Authenticatable
+class Admin extends Authenticatable
+{
+    use Notifiable;
+    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $table = 'admins';
+    
+    protected $fillable = [
+    'name', 'email', 'password',
+    ];
+    
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+    
+    /**
+     * Send the password reset notification.
+     *
+     * @param  string  $token
+     * @return void
+     */
+    public function sendPasswordResetNotification($token)
     {
-        use Notifiable;
-    
-        /**
-         * The attributes that are mass assignable.
-         *
-         * @var array
-         */
-        protected $table = 'admins';
-    
-        protected $fillable = [
-            'name', 'email', 'password',
-        ];
-    
-        /**
-         * The attributes that should be hidden for arrays.
-         *
-         * @var array
-         */
-        protected $hidden = [
-            'password', 'remember_token',
-        ];
-    
-        /**
-         * Send the password reset notification.
-         *
-         * @param  string  $token
-         * @return void
-         */
-        public function sendPasswordResetNotification($token)
-        {
-            $this->notify(new AdminResetPassword($token));
-        }
+        $this->notify(new AdminResetPassword($token));
     }
+}
--- app/Karyawan.php
+++ app/Karyawan.php
@@ -1,50 +1,50 @@
-    <?php
+<?php
     
-    namespace App;
+namespace App;
     
-    use App\Notifications\KaryawanResetPassword;
-    use Illuminate\Notifications\Notifiable;
-    use Illuminate\Foundation\Auth\User as Authenticatable;
+use App\Notifications\KaryawanResetPassword;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
     
-    class Karyawan extends Authenticatable
+class Karyawan extends Authenticatable
+{
+    use Notifiable;
+    
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $table = 'karyawan';
+    
+    protected $fillable = [
+        'gambar', 'asal', 'foto',
+        'alamat', 'tanggal_lahir', 'no',
+        'name', 'email', 'password', 'cabang_id'
+    ];
+    
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember',
+    ];
+    
+    /**
+     * Send the password reset notification.
+     *
+     * @param  string  $token
+     * @return void
+     */
+    public function sendPasswordResetNotification($token)
     {
-        use Notifiable;
+        $this->notify(new KaryawanResetPassword($token));
+    }
     
-        /**
-         * The attributes that are mass assignable.
-         *
-         * @var array
-         */
-        protected $table = 'karyawan';
-    
-        protected $fillable = [
-            'gambar', 'asal', 'foto',
-            'alamat', 'tanggal_lahir', 'no',
-            'name', 'email', 'password', 'cabang_id'
-        ];
-    
-        /**
-         * The attributes that should be hidden for arrays.
-         *
-         * @var array
-         */
-        protected $hidden = [
-            'password', 'remember',
-        ];
-    
-        /**
-         * Send the password reset notification.
-         *
-         * @param  string  $token
-         * @return void
-         */
-        public function sendPasswordResetNotification($token)
-        {
-            $this->notify(new KaryawanResetPassword($token));
-        }
-    
-        public function cabang()
-        {
-            return $this->belongsTo('App\Cabang');
-        }
+    public function cabang()
+    {
+        return $this->belongsTo('App\Cabang');
+    }
     }
